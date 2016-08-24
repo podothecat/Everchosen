61222094b977c06a62857c93471564e298bfd76d
munjunyoung <sswsswsswss@gmail.com>
2016-08-25 00:46:44 +0900

    Update Profile NickName

--- EverchosenProject/Assets/Script/AllScene/TribeSetManager.cs
+++ EverchosenProject/Assets/Script/AllScene/TribeSetManager.cs
@@ -16,13 +16,15 @@ public class PlayerData
 {
   
     public string UserID { get; set; }
+    public string NickName { get; set; }
     public int Tribe { get; set; }
     public string TribeName { get; set; }
     public int Spell { get; set; }
 
 
-    public PlayerData(string userid,int tribe, string tribeName, int spell)
+    public PlayerData(string userid,string nickName, int tribe, string tribeName, int spell)
     {
+        this.NickName = nickName;
         this.Tribe = tribe;
         this.UserID = userid;
         this.TribeName = tribeName;
